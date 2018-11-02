b450e8120c032bb2e38c089c92924ed6b572308d
Unknown <louison.harizi@epitech.eu>
2018-11-02 01:43:09 +0100

    player move + test static follow

---
+++ Assets/follow_move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_move : MonoBehaviour {
+
+    public GameObject leader;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, leader.transform.position, 0.25f); //0.5f
+    }
+}
---
+++ Assets/player_move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(6, 6);
+    private Vector2 movement;
+
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+                speed.x * inputX,
+                speed.y * inputY);
+
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
