9b36ac899e95e6e749e6fa51090e815790f83d88
Unknown <louison.harizi@epitech.eu>
2018-11-05 01:51:02 +0100

    spawn follow

--- Assets/player_move.cs
+++ Assets/player_move.cs
@@ -7,6 +7,36 @@ public class player_move : MonoBehaviour {
     public Vector2 speed = new Vector2(6, 6);
     private Vector2 movement;
 
+    public GameObject newFollow;
+
+    List<GameObject> followList;
+
+    private void Start()
+    {
+        followList = new List<GameObject>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("spawner"))
+        {
+            GameObject neww;
+            neww = Instantiate(newFollow, transform.position, Quaternion.identity);
+            followList.Add(neww);
+
+            for (int i = 0; i < followList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    followList[i].GetComponent<follow_move>().leader = this.gameObject;
+                }
+                else
+                {
+                    followList[i].GetComponent<follow_move>().leader = followList[i - 1];
+                }
+            }
+        }
+    }
 
     void Update()
     {
